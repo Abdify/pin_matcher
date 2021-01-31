2e45940ddfbcffbcc46ac7aa169e275f06faba5f
Abdify <abdullah.fahimm08@gmail.com>
2021-01-31 11:34:43 +0600

    backspace, clear etc added

--- action.js
+++ action.js
@@ -2,6 +2,7 @@ let generateBtn = document.querySelector('.generate-btn');
 let generatedPin = document.querySelector('#generatedPinDisplay');
 let newPin = 0;
 
+//Generate random pin
 generateBtn.addEventListener('click', () => {
     newPin = Math.round(1000 + Math.random()*(9000));
     console.log(newPin);
@@ -12,16 +13,28 @@ generateBtn.addEventListener('click', () => {
 
 let userPinDisplay = document.querySelector('#userPinDisplay');
 let numberBtns = document.querySelector('.calc-body');
-
 document.onload = userPinDisplay.value = '';
-
+//Getting user input
 numberBtns.addEventListener('click', (e) => {
+    userPinDisplay.style.border = '';
+
     let digit = e.target.innerText;
+    //Submit
     if(e.target.type === 'submit' && newPin !== 0){
         matchPin(userPinDisplay.value);
         userPinDisplay.value = '';
     }
-    else if(/\d/.test(digit) && digit.length === 1){
+    //Clear
+    else if(digit === 'C'){
+        userPinDisplay.value = '';
+    }
+    //backspace
+    else if(digit === '<'){
+        let x = userPinDisplay.value.slice(0, -1);
+        userPinDisplay.value = x;
+    }
+    //Number
+    else if(!isNaN(digit)){
 
         userPinDisplay.value += digit;
     }
@@ -29,20 +42,20 @@ numberBtns.addEventListener('click', (e) => {
 
 
 function matchPin(userPin){
-    console.log(userPin);
     userPin = Number(userPin);
     if(userPin === newPin){
         console.log('Success!')
         document.querySelector('.notify:nth-child(1)').style.display = 'none';
         document.querySelector('.notify:nth-child(2)').style.display = 'block';
+        newPin = 0;
+        generatedPin.value = '';
     }
     else{
         console.log('Intruder!!!!');
         document.querySelector('.notify:nth-child(2)').style.display = 'none';
         document.querySelector('.notify:nth-child(1)').style.display = 'block';
+        userPinDisplay.style.border = '2px solid red';
     }
     
-    newPin = 0;
-    generatedPin.value = '';
+    
 }
-
